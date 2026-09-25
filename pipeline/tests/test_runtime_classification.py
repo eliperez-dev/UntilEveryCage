2c324468dc7723f9b150c8321057907d62022fd3
Eli Perez <pnaeli2006@gmail.com>
2026-09-25 13:53:34 -0700

    Fix integrated private source classification hygiene

--- pipeline/tests/test_runtime_classification.py
+++ pipeline/tests/test_runtime_classification.py
@@ -10,6 +10,7 @@ from pipeline.source_runtime_classification import (
 VERIFIED_PRIVATE_E2E = {
     "au.sa.epa.licensed-activities",
     "be.locations",
+    "ca.cfia.federal-meat",
     "dk.smiley",
     "fr.dgal.section-i",
     "fr.dgal.section-ii",
